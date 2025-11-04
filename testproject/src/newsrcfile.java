88aae4698d8231d769b15202e9bb6dd875a848de
BK <guitarkirb@gmail.com>
2025-11-04 17:23:51 -0500

    Initial Commit

---
+++ testproject/src/newsrcfile.java
@@ -0,0 +1,13 @@
+public class newsrcfile {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+			int num1 = 5;
+			int num2 = 7;
+			int total = num1 + num2;
+			
+			System.out.println(total);
+			
+	}
+
+}

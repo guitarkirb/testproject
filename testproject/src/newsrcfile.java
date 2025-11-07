b9218689f44920e1bb134d7609da6c7b39ad3a50
BK <guitarkirb@gmail.com>
2025-11-07 14:27:31 -0500

    initial file commit

--- testproject/src/newsrcfile.java
+++ testproject/src/newsrcfile.java
@@ -3,7 +3,7 @@ public class newsrcfile {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 			int num1 = 5;
-			int num2 = 7;
+			int num2 = 6;
 			int total = num1 + num2;
 			
 			System.out.println(total);
